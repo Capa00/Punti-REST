adfec35788fcfbe7dd46693d054b086a502ec894
Giuseppe sp <g.dipuglia@pharmawizard.it>
2021-12-04 17:24:02 +0100

    organizing settings

--- punti/settings/auth.py
+++ punti/settings/auth.py
@@ -1,11 +1,11 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8b(a(^v&f#is4b+0$&#mpki38$i+#d3=35!^r#lww5%7*4slh2'
 
 ALLOWED_HOSTS = []
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-8b(a(^v&f#is4b+0$&#mpki38$i+#d3=35!^r#lww5%7*4slh2'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
